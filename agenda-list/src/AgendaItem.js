2eb23c07b5371065cb810ac350b5602c5d1ea30e
Rony Kaseyani <rony.kasbuh@gmail.com>
2020-12-03 12:24:36 +0100

    created AgendaItem component

---
+++ agenda-list/src/AgendaItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AgendaItem() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text</p>
+        </div>
+    )
+}
+
+export default AgendaItem
--- agenda-list/src/App.js
+++ agenda-list/src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import AgendaItem from "./AgendaItem";
 
 function App() {
   return (
     <div className="App">
-        <input type="checkbox"/>
-        <p>Placeholder Text</p>
-        <input type="checkbox"/>
-        <p>Placeholder Text</p>
-        <input type="checkbox"/>
-        <p>Placeholder Text</p>
-        <input type="checkbox"/>
-        <p>Placeholder Text</p>
-        <input type="checkbox"/>
-        <p>Placeholder Text</p>                                
+    <React.StrictMode>
+      <AgendaItem />
+      <AgendaItem />
+      <AgendaItem />
+      <AgendaItem />
+      <AgendaItem />
+    </React.StrictMode>
     </div>
   );
 }
