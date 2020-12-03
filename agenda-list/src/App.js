b5800f1b00508e76e200b5c2b6b2e0c1cb9ced90
Rony Kaseyani <rony.kasbuh@gmail.com>
2020-12-03 11:42:45 +0100

    boiler plate code & initial app setup

---
+++ agenda-list/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <input type="checkbox"/>
+        <p>Placeholder Text</p>
+        <input type="checkbox"/>
+        <p>Placeholder Text</p>
+        <input type="checkbox"/>
+        <p>Placeholder Text</p>
+        <input type="checkbox"/>
+        <p>Placeholder Text</p>
+        <input type="checkbox"/>
+        <p>Placeholder Text</p>                                
+    </div>
+  );
+}
+
+export default App;
