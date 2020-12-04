908490d8493316e642ca358f837380b1e771c1b6
Rony Kaseyani <rony.kasbuh@gmail.com>
2020-12-04 18:24:28 +0100

    added an event handler and state to the App

--- agenda-list/src/AgendaItem.js
+++ agenda-list/src/AgendaItem.js
@@ -3,8 +3,12 @@ import React from "react";
 function AgendaItem(props) {
   return (
     <div>
-      <input type="checkbox" checked={props.completed} />
-      <p>{props.text}</p>
+      <input
+        type="checkbox"
+        checked={props.agenda.completed}
+        onChange={() => props.handleChange(props.agenda.id)}
+      />
+      <p>{props.agenda.text}</p>
       <hr />
     </div>
   );
--- agenda-list/src/App.js
+++ agenda-list/src/App.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AgendaItem from "./AgendaItem";
 
-import agendaData from "./agendaData";
+//import agendaData from "./agendaData";
 
 function App() {
-  const agendaList = agendaData.map((agenda) => {
-    return <AgendaItem text={agenda.text} completed={agenda.completed} />;
+  const [list, setList] = useState({
+    agendaData: [
+      {
+        id: 1,
+        text: "Do the laundry",
+        completed: true,
+      },
+      {
+        id: 2,
+        text: "Do the dishes",
+        completed: false,
+      },
+      {
+        id: 3,
+        text: "Vacuum and mop",
+        completed: false,
+      },
+      {
+        id: 4,
+        text: "Take out trash",
+        completed: true,
+      },
+      {
+        id: 5,
+        text: "Watch 'This is Us'",
+        completed: false,
+      },
+    ],
+  });
+
+  function handleChange(id) {
+    console.log(id);
+
+    let updatedList = list.agendaData.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      }
+      return item;
+    });
+    setList({
+      agendaData: updatedList,
+    });
+  }
+
+  const agendaList = list.agendaData.map((agenda) => {
+    return (
+      <AgendaItem key={agenda.id} agenda={agenda} handleChange={handleChange} />
+    );
   });
 
   return (
